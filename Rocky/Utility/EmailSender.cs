399fae51fcad35bcf81d8ac520812e80e8bc163e
Dinara <sinatranur3462@gmail.com>
2021-04-21 21:55:29 +0600

    first commit

---
+++ Rocky/Utility/EmailSender.cs
@@ -0,0 +1,62 @@
+﻿using Mailjet.Client;
+using Mailjet.Client.Resources;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rocky.Utility
+{
+    public class EmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            return Execute(email, subject, htmlMessage);
+        }
+
+        public async Task Execute(string email, string subject, string body)
+        {
+            MailjetClient client = new MailjetClient("c5c4603bc18209b72e509f3cc50c898b", "b54a40e59563bab9394f7ac663f58d57")
+            {
+                /*Version = ApiVersion.V3_1,*/
+            };
+            MailjetRequest request = new MailjetRequest
+            {
+                Resource = Send.Resource,
+            }
+             .Property(Send.Messages, new JArray {
+     new JObject {
+      {
+       "From",
+       new JObject {
+        {"Email", "sinatranur@protonmail.com"},
+        {"Name", "Dinara"}
+       }
+      }, {
+       "To",
+       new JArray {
+        new JObject {
+         {
+          "Email",
+          email
+         }, {
+          "Name",
+          "DotNetMastery"
+         }
+        }
+       }
+      }, {
+       "Subject",
+       subject
+      },  {
+       "HTMLPart",
+        body      
+      }, 
+     }
+             });
+             await client.PostAsync(request);
+        }
+    }
+}
